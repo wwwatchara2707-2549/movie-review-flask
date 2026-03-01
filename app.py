b02afd0aff4c6c2d9440012331db73045c5c7b8d
Watcharapon Warapeang <wwwatchara2707@gmail.com>
2026-03-01 15:08:06 +0700

    Add Contact page route and template

--- app.py
+++ app.py
@@ -228,5 +228,10 @@ def delete_movie(id):
 def about():
     return render_template("about.html")
 
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
