9791e59b9efac7bb1ad7f4e74c184af28cab31b5
Watcharapon Warapeang <wwwatchara2707@gmail.com>
2026-03-01 15:07:04 +0700

    Add About page route and template

--- app.py
+++ app.py
@@ -223,5 +223,10 @@ def delete_movie(id):
 # ----------------------------
 # LOCAL RUN
 # ----------------------------
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
